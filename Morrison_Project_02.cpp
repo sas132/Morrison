9dde83c823613161c65596cb82da20d1fc66e165
Sarah Stevenson <sas132@students.uwf.edu>
2019-10-11 16:01:58 -0500

    Bofa

--- Morrison_Project_02.cpp
+++ Morrison_Project_02.cpp
@@ -10,7 +10,7 @@ int main ()
 do{
 	do
 	{
-		cout << "Enter the vehicle of your choosing. ";
+		cout << "Which vehicle are you interested in? (enter E, S, L, V or T): ";
 		cin >> input_char;
 		input_correct = true;
 		
@@ -47,20 +47,26 @@ do{
 			Overage_Fee = 0.50;
 			break;
 		default:
+			cout << "Enter only E, S, L, V or T." << endl;
 			input_correct = false;
 			break;
 		}			
 	
-	} while (input_correct = false);
+	} while (input_correct == false);
 do
 {
-	cout << "Enter how many miles you plan to use. ";
+	input_correct = false;
+	cout << "How many miles do you estimate you'll drive? (1-812): ";
 	cin >> input_num;
-	if (0 > input_num || 813 < input_num)
+	if (1 <= input_num && 812 >= input_num)
 	{
-		input_correct = false;
+		input_correct = true;
 	}
-} while (input_correct = false);
+	else
+	{
+		cout << "Please enter a number between 1 and 812, inclusive." << endl;
+	}
+} while (input_correct == false);
 double economy_daily = 45.90;
 double sport_daily = 57.75;
 double luxury_daily = 85.50;
@@ -76,18 +82,61 @@ double sport_overage_fee = 0.40;
 double luxury_overage_fee = 0.35;
 double van_overage_fee = 0.50;
 double truck_overage_fee = 0.50;
-double economy_total = economy_daily + (input_num - economy_miles) * economy_overage_fee;
-double sport_total = sport_daily + (input_num - sport_miles) * sport_overage_fee;
-double luxury_total = luxury_daily + (input_num - luxury_miles) * luxury_overage_fee;
-double van_total = van_daily + (input_num - van_miles) * van_overage_fee;
-double truck_total = truck_daily + (input_num - truck_miles) * truck_overage_fee;
-double input_total = Daily_Fee + (input_num - Free_Miles) * Overage_Fee;
+double economy_total, sport_total, luxury_total, van_total, input_total, truck_total;
+if(economy_miles >= input_num)
+{
+economy_total = economy_daily;
+}
+else
+{
+economy_total = economy_daily + (input_num - economy_miles) * economy_overage_fee;
+}
+if(sport_miles >= input_num)
+{
+sport_total = sport_daily;
+}
+else
+{
+ sport_total = sport_daily + (input_num - sport_miles) * sport_overage_fee;
+}
+if (luxury_miles >= input_num)
+{
+luxury_total = luxury_daily;
+}
+else
+{
+luxury_total = luxury_daily + (input_num - luxury_miles) * luxury_overage_fee;
+}
+if (van_miles >= input_num)
+{
+van_total = van_daily;
+}
+else
+{
+van_total = van_daily + (input_num - van_miles) * van_overage_fee;
+}
+if (truck_miles >= input_num)
+{
+truck_total = truck_daily;
+}
+else
+{
+truck_total = truck_daily + (input_num - truck_miles) * truck_overage_fee;
+}
+if (Free_Miles >= input_num)
+{
+input_total = Daily_Fee;
+}
+else
+{
+input_total = Daily_Fee + (input_num - Free_Miles) * Overage_Fee;
+}
 double economy_difference = input_total - economy_total;
 double sport_difference = input_total - sport_total;
 double luxury_difference = input_total - luxury_total;
 double van_difference = input_total - van_total;
 double truck_difference = input_total - truck_total;
-cout << input_total << endl;
+cout << "The charges are $" << input_total << endl;
 if (economy_difference > 0)
 {
 	cout << "By switching to the Economy vehicle you would save $" << economy_difference << endl;
@@ -111,7 +160,8 @@ if (truck_difference > 0)
 	cout << "Do you want to go again (y/n)? ";
 	cin >> exit_char;
 
-}while(exit_char == 'y');
+}while(exit_char == 'y'||exit_char == 'Y' );
+cout << "Thank you for using this program. Goodbye." << endl;
 return 0;
 }
 
